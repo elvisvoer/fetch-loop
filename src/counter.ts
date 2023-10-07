85a8dae0d5929ce041b8eefd95dc8832b701795a
Elvis <elvis.adomnica@gmail.com>
2023-10-07 18:24:50 +0200

    Initial commit

---
+++ src/counter.ts
@@ -0,0 +1,11 @@
+export async function setupCounter(element: HTMLButtonElement) {
+  let counter = 0;
+  const setCounter = (count: number) => {
+    counter = count;
+    element.innerHTML = `count is ${counter}`;
+  };
+  element.addEventListener("click", () => setCounter(counter + 1));
+  setCounter(0);
+
+  setTimeout(stop, 60_000);
+}
